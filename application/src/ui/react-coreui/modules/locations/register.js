df42a8f8ca5fa8d5325da838d670321d363daf75
Kevin Chevalier <kchevalier@Quimby.local>
2020-08-23 22:55:02 -0700

    remove core UI files and update dependencies

--- application/src/ui/react-coreui/modules/locations/register.js
+++
@@ -1,9 +0,0 @@
-import Logger from '../../../../lib/Logger';
-
-const register = () => {
-  Logger.log('debug', `locations.register()`);
-}
-
-export default register;
-
-Logger.log('silly', `locations.register() loaded.`);
--- application/src/ui/react-coreui/modules/session/register.js
+++
@@ -1,9 +0,0 @@
-import Logger from '../../../../lib/Logger';
-
-const register = () => {
-  Logger.log('debug', `session.register()`);
-}
-
-export default register;
-
-Logger.log('silly', `session.register() loaded.`);
